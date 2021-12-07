b4c2c9d5149deb490ea922a971837df137475ab5
Makarand Lahane <lahanem@mymail.shawnee.edu>
2021-12-07 10:04:39 -0500

    Disable the email confirmation

--- graphql/resolvers/user/mutations/register.js
+++ graphql/resolvers/user/mutations/register.js
@@ -1,76 +1,79 @@
-const { UserInputError } = require('apollo-server-express');
-const bcrypt = require('bcryptjs');
-const { validateRegisterInput } = require('../../../../util/validators');
+const { UserInputError } = require("apollo-server-express");
+const bcrypt = require("bcryptjs");
+const { validateRegisterInput } = require("../../../../util/validators");
 const nodemailer = require("nodemailer");
-const jwt = require('jsonwebtoken');
-const { SECRET_KEY } = require('../../../../config');
-
+const jwt = require("jsonwebtoken");
+const { SECRET_KEY } = require("../../../../config");
 
 const transporter = nodemailer.createTransport({
-    service: 'Gmail',
-    auth: {
-        user: process.env.GMAIL_USER,
-        pass: process.env.GMAIL_PASS
-    }
+  service: "Gmail",
+  auth: {
+    user: process.env.GMAIL_USER,
+    pass: process.env.GMAIL_PASS,
+  },
 });
 
-
-
 module.exports = {
-    Mutation: {
-        async register(parent, {
-            registerInput: {
-                username,
-                email,
-                password,
-                confirmPassword
-            }
-        }, context, info) {
-            const { errors, valid } = validateRegisterInput(username, email, password, confirmPassword);
-            if (!valid) {
-                throw new UserInputError('Errors', {
-                    errors
-                });
-            }
+  Mutation: {
+    async register(
+      parent,
+      { registerInput: { username, email, password, confirmPassword } },
+      context,
+      info
+    ) {
+      const { errors, valid } = validateRegisterInput(
+        username,
+        email,
+        password,
+        confirmPassword
+      );
+      if (!valid) {
+        throw new UserInputError("Errors", {
+          errors,
+        });
+      }
 
-            const existingUser = await context.prisma.user.findUnique({ where: { email } });
-            if (existingUser) {
-                throw new UserInputError("Username is taken", {
-                    errors: {
-                        username: 'Please confirm your account'
-                    }
-                });
-            }
-            password = await bcrypt.hash(password, 12);
+      const existingUser = await context.prisma.user.findUnique({
+        where: { email },
+      });
+      if (existingUser) {
+        throw new UserInputError("Username is taken", {
+          errors: {
+            username: "Please confirm your account",
+          },
+        });
+      }
+      password = await bcrypt.hash(password, 12);
 
-            const newUser = await context.prisma.user.create({
-                data: {
-                    username,
-                    email,
-                    password
-                }
-            });
+      const newUser = await context.prisma.user.create({
+        data: {
+          username,
+          email,
+          password,
+          confirmed: true,
+        },
+      });
 
-            jwt.sign(
-                {
-                    id: newUser.id,
-                    username: newUser.username
-                },
-                SECRET_KEY,
-                {
-                    expiresIn: '1d',
-                },
-                (err, emailToken) => {
-                    const url = `http://localhost:3000/confirmation/${emailToken}`;
-                    console.log(url);
-                    transporter.sendMail({
-                        to: email,
-                        subject: 'Confirm Email',
-                        html: `Please click this email to confirm your email: <a href="${url}">${url}</a>`,
-                    });
-                },
-            );
-            return newUser;
+      jwt.sign(
+        {
+          id: newUser.id,
+          username: newUser.username,
+        },
+        SECRET_KEY,
+        {
+          expiresIn: "1d",
+        },
+        (err, emailToken) => {
+          const url = `http://localhost:3000/confirmation/${emailToken}`;
+          console.log(url);
+          transporter.sendMail({
+            to: email,
+            subject: "Confirm Email",
+            html: `Please click this email to confirm your email: <a href="${url}">${url}</a>`,
+          });
         }
-    }
+      );
+      return newUser;
+    },
+  },
 };
